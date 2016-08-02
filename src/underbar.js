a5128ecbf510ce4935151fd109e7ce7a5ab0533f
Cheney Mao <cheneymao94@gmail.com>
2016-08-01 19:05:07 -0700

    Implement _.filter

--- src/underbar.js
+++ src/underbar.js
@@ -78,6 +78,15 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+
+    _.each(collection, function(elem) {
+      if (test(elem)) {
+        result.push(elem);
+      }
+    });
+    
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
